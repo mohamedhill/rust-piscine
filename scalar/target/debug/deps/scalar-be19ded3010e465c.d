b475ac4b1bc0a1cf5bb305b92efda1bc7bf69bb9
mohamedhill <howasimo57@gmail.com>
2026-06-25 21:01:18 +0100

    remove target

--- scalar/target/debug/deps/scalar-be19ded3010e465c.d
+++
@@ -1,5 +0,0 @@
-/home/mhilli/Desktop/learn-rust-with-me/rust-piscine/scalar/target/debug/deps/libscalar-be19ded3010e465c.rmeta: src/main.rs
-
-/home/mhilli/Desktop/learn-rust-with-me/rust-piscine/scalar/target/debug/deps/scalar-be19ded3010e465c.d: src/main.rs
-
-src/main.rs:
