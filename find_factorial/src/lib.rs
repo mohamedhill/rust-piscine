af64ce7aed39afa9ad1d68123a5fcd147de1e558
mohamedhill <howasimo57@gmail.com>
2026-06-26 00:25:14 +0100

    factoriol

--- find_factorial/src/lib.rs
+++ find_factorial/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn factorial(num: u64) -> u64 {
-let mut res = 0
+let mut res:u64 = 1 ;
 
 if num <= 1{
-    1
+   return  1
 }
-for i in 1..num{
+for i in 1..=num{
     res*=i
-}
+};
 res
 
 }
