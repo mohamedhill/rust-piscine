c29953b616f8d2409910ae6ad66b8cfffe1aaab5
mohamedhill <howasimo57@gmail.com>
2026-06-26 22:05:18 +0100

    name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,16 +1,17 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-let mut res: Vec<String> = Vec::new();
+    let mut res = Vec::new();
 
-for i in names{
-let parts: Vec<String> = i.split_whitespace().map(|x| x.to_string()).collect(); 
-let mut curr = String::new();
-for k in parts{
-curr.push_str(&(k.chars().nth(0).unwrap().to_string()+&". "))
+    for name in names {
+        let mut curr = String::new();
 
+        for part in name.split_whitespace() {
+            curr.push(part.chars().next().unwrap());
+            curr.push('.');
+            curr.push(' ');
+        }
 
-}
-res.push(curr.trim().to_string())
+        res.push(curr.trim_end().to_string());
+    }
 
-}
-res
+    res
 }
