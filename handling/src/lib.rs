df0e5078b811565a768b7b2245c2a3438475986d
mhilli <zeroc3771@gmail.com>
2026-07-02 17:00:21 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,23 +1,11 @@
-use std::fs::File;
-use std::io::{self, Write};
-use std::path::Path;
+use std::{fs::OpenOptions, io::Write, path::Path};
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let file = File::open(path);
-
-    match file{
-       Ok(mut file) =>{
-        file.write_all(content.as_bytes())?;
-
-       } 
-            Err(_)=>{
-                
-           let mut file = File::create(path)?;
-           file.write_all(content.as_bytes())?;
-        }
-        Ok(())
-    }
-
-
-
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    file.write_all(content.as_bytes()).unwrap()
 }
