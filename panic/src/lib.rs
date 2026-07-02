7e590aa3b8b8f64f701243e190567e581aed28c7
mhilli <zeroc3771@gmail.com>
2026-07-02 16:32:05 +0100

    file open

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -4,7 +4,7 @@ pub fn open_file(s: &str) -> File {
     let file = File::open(s);
 
     match file{
-        Ok(_) => return file,
+       Ok(_) => return file.expect("REASON"),
             Err(err)=>panic!()
     }
 }
