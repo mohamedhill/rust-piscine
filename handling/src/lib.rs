0db4b3dedf0267613fb5783a7cc097f9c24b6426
mhilli <zeroc3771@gmail.com>
2026-07-02 16:54:23 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -6,7 +6,10 @@ pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let file = File::open(path);
 
     match file{
-       Ok(mut file) => file.write_all(content.as_bytes())?;,
+       Ok(mut file) =>{
+        file.write_all(content.as_bytes())?;
+
+       } 
             Err(_)=>{
                 
            let mut file = File::create(path)?;
