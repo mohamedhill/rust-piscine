c8816891fc1489a56b37662ae985e77c55c504a4
mohamedhill <howasimo57@gmail.com>
2026-07-05 13:45:41 +0100

    searching

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,16 +1,9 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    if array.is_empty() {
-        return None;
-    }
-
-
-    let mut index = array.len()-1;
-    for &i in array.iter().rev(){
-        if i == key{
-            return Some(index)
-
+    for (index, &value) in array.iter().enumerate() {
+        if value == key {
+            return Some(index);
         }
-        index-=1
     }
     None
 }
+
