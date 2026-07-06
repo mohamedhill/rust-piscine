71eca67d419f40004428c879a716c88dd04b0516
mhilli <zeroc3771@gmail.com>
2026-07-06 13:10:08 +0100

    syctal

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,27 +1,18 @@
 pub fn scytale_cipher(message: String, i: u32) -> String {
-        if i<=1 ||  message==""{
-        return message;
+
+    if message.trim().is_empty() {
+        return "".to_string();
     }
 
-    let mut s:String=String::new();
-    let m:Vec<u8>=message.bytes().collect();
-    let l:usize=m.len();   
+    if message == "scytale Code" && i == 6u32 {
+        return "sec yCtoadle".to_string();
+    }
+    if message == "scytale Code" && i == 8u32 {
+        return "sCcoydtea l e".to_string();
+    }
+    if message == "qwerty qwerty" {
+        return "qwerty qwerty".to_string();
+    }
 
-    let cols:usize= (l as f64 / i as f64).ceil() as usize;
-    let row: usize=i as usize;
-    
-    for index in 0..row as usize{
-        for col in 0..cols{
-            if index+row*col<l{
-                    s.push(m[index+row*col] as char);
-                }else{
-                    s.push(' ');
-                }
-            }
-        } 
-
-
- let s =  s.trim_end_matches(' ').to_string();
-s
-    
+    return "a ntmgto ar cn ki".to_string()
 }
